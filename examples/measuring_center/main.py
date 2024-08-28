8d198d1d5d7007f6c4208df14eea2150721dbff5
HiraiKyo <kyo.mikhael@gmail.com>
2024-08-28 20:03:04 +0900

    算出例追加

--- examples/measuring_center/main.py
+++ examples/measuring_center/main.py
@@ -1,18 +1,33 @@
+from pprint import pprint
 import open3d as o3d
 import numpy as np
 from ply_processor_basics.points.convex_hull import detect_line
 from ply_processor_basics.points.ransac import detect_plane
 from ply_processor_basics.points.clustering import plane_clustering
-from ply_processor_basics.points.convex_hull.detect_circle import fit_circle
+from ply_processor_basics.points.convex_hull import detect_circle
+
 def visualize(points):
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(points)
     o3d.visualization.draw_geometries([pcd])
 
+def line_distance(p0, v0, p1, v1):
+    v0 = v0 / np.linalg.norm(v0)
+    v1 = v1 / np.linalg.norm(v1)
+
+    w0 = p0 - p1
+    a = np.cross(v0, v1)
+    b = np.linalg.norm(a)
+
+    if b == 0:
+        return np.linalg.norm(np.cross(w0, v0))
+
+    return abs(np.dot(w0, a)) / b
+
 def main():
     pcd = o3d.io.read_point_cloud("sample.ply")
-    # o3d.visualization.draw_geometries([pcd], window_name="Original Point Cloud")
     points = np.asarray(pcd.points)
+    # visualize(points)
 
     # エッジ検出
     plane_indices, plane_model = detect_plane(points, threshold=1.0)
@@ -38,16 +53,32 @@ def main():
     if len(clusters) == 0:
         return
     points_cylinder_top = tmp[plane_indices][clusters[0]]
-    print(points_cylinder_top.shape)
+    # visualize(points_cylinder_top)
+
     # 円盤フィッティング
-    center, normal, radius = fit_circle(points_cylinder_top)
+    indices, center, normal, radius = detect_circle(points_cylinder_top, plane_model)
 
     # 円盤とエッジを描画
-    mesh_disk = o3d.geometry.TriangleMesh.create_cylinder(radius=radius, height=0.1, resolution=50, split=1)
+    mesh_disk = o3d.geometry.TriangleMesh.create_cylinder(radius=radius, height=3.0, resolution=50, split=1)
     mesh_disk.compute_vertex_normals()
+    normal = normal / np.linalg.norm(normal)
+    rotation_axis = np.cross(np.array([0, 0, 1]), normal)
+    rotation_angle = np.arccos(np.dot(np.array([0, 0, 1]), normal))
+    rotation_matrix = mesh_disk.get_rotation_matrix_from_axis_angle(rotation_axis * rotation_angle)
+    mesh_disk.rotate(rotation_matrix, center=(0, 0, 0))
     mesh_disk.translate(center)
-    mesh_disk.rotate(mesh_disk.get_rotation_matrix_from_xyz((np.pi/2, 0, 0)), center)
     o3d.visualization.draw_geometries([line_set, pcd, mesh_disk])
+
+    # 各エッジと円筒軸の間の距離を算出
+    distances = []
+    for line_model in line_models:
+        p, v = line_model
+        distances.append(line_distance(center, normal, p, v))
+    print("円筒中心: ", center)
+    print("円筒半径: ", radius)
+    # 配列をpretty print
+    print("エッジと円筒軸の距離: ")
+    pprint([f"{d:.2f}" for d in distances])
 
 if __name__ == "__main__":
     main()
